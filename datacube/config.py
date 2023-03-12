ebb37f5c68ab0f0fc161ea159b3a0dc0412a6d64
Petr Borňás <petriczech12@gmail.com>
2023-03-12 22:49:58 +0100

    Add Care Providers dataset

--- datacube/config.py
+++ datacube/config.py
@@ -1,4 +1,17 @@
 from pathlib import Path
 from typing import Annotated
 
+BASE: Annotated[
+    str, "Base path of local RDF data's IRIs."
+] = "https://bugulin.github.io/ndbi046"
+
 DATA_DIR: Annotated[Path, "Path where to save downloaded files."] = Path(".data/")
+
+PUBLISHER: Annotated[
+    str, "Publisher of the generated data cubes."
+] = "https://bugulin.github.io/"
+
+
+CARE_PROVIDERS_URL: Annotated[
+    str, "URL of the Care Providers dataset."
+] = "https://opendata.mzcr.cz/data/nrpzs/narodni-registr-poskytovatelu-zdravotnich-sluzeb.csv"
---
+++ datacube/namespace.py
@@ -0,0 +1,9 @@
+from rdflib import Namespace
+
+from .config import BASE
+
+CODE = Namespace(f"{BASE}/code#")
+ONTOLOGY = Namespace(f"{BASE}/ontology#")
+RESOURCE = Namespace(f"{BASE}/resource/")
+
+SDMX_SUBJECT = Namespace("http://purl.org/linked-data/sdmx/2009/subject#")
